73818df93675efc4593bcdf60519a02eceef12e3
harshasaiseran <93912552+harshasaiseran@users.noreply.github.com>
2022-02-21 11:48:41 -0500

    Add files via upload

---
+++ Number is +ve or -ve or zero.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter any number");
+    scanf("%d",&a);
+    if(a>0)
+    printf("the number is positive");
+    else if(a<0)
+    printf("the number is negative");
+    else
+    printf("the number is zero");
+
+    return 0;
+}
+
+
