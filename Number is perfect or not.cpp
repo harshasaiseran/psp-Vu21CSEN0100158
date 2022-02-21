f1e01cbe7d8fda176789d85360f86494603880d0
harshasaiseran <93912552+harshasaiseran@users.noreply.github.com>
2022-02-21 11:56:54 -0500

    Add files via upload

---
+++ Number is perfect or not.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num,rem,sum=0,i;
+    printf("enter the number\n");
+    scanf("%d",&sum);
+    for(i=1;i<num;i++)
+    {
+        rem=num%i;
+        if(rem==0)
+     {
+        sum=sum+i;
+     }
+    }
+    if(sum=sum){
+        printf("%d is a perfect num");
+    }
+    else
+    printf("%d is not a perfect num");
+    return 0;
+}
+
