d349ff71d818780da9c41f6fa411315b914da135
harshasaiseran <93912552+harshasaiseran@users.noreply.github.com>
2022-02-21 11:46:46 -0500

    Add files via upload

---
+++ Number divisible by 5 and 11.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    if(a%5==0)
+    printf("a is divisible by 5");
+    else
+    printf("a is not divisible by 5");
+    if(a%11==0)
+    printf("\na is dividsible by 11");
+    else
+    printf("\na is not divisible by 11");
+
+    return 0;
+}
+
+
+
